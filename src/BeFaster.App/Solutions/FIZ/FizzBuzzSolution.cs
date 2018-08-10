88d5a9c655946c354ff07c830dfd358e64103779
sbx_user1060 <sbx_user1060@ip-10-11-179-185.eu-west-2.compute.internal>
2018-08-10 23:15:56 +0000

    Fri Aug 10 23:15:56 UTC 2018

--- src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -7,8 +7,12 @@ namespace BeFaster.App.Solutions.FIZ
         public static string fizz_buzz(int number)
         {
             var result = "";
-            if (((number % 3) == 0) || number.ToString().Contains("3")) result = "fizz ";
+            var deluxe = false;
+            if (((number % 3) == 0) || number.ToString().Contains("3")) result = "fizz "; 
             if (((number % 5) == 0) || number.ToString().Contains("5")) result += "buzz ";
+            if ((number % 3) == 0 && number.ToString().Contains("3")) deluxe = true;
+            if ((number % 5) == 0 && number.ToString().Contains("5")) deluxe = true;
+            if (deluxe) result += "deluxe";
             if ((number > 10 && number.ToString().Replace(number.ToString()[0].ToString(), "") == "")) result += "deluxe";
             if (result.Contains("deluxe") && (number % 2) != 0) result=result.Replace("deluxe", "fake deluxe");
             if (result == "") result = number.ToString();
